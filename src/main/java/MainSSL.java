df3d9621cbf59568d7cd064a9cd5524b7bf4f9ff
Phillip O <phille.ortiz@gmail.com>
2016-12-13 17:12:37 +0100

    Simple startup

---
+++ src/main/java/MainSSL.java
@@ -0,0 +1,26 @@
+
+import java.io.IOException;
+import java.security.KeyManagementException;
+import java.security.KeyStoreException;
+import java.security.NoSuchAlgorithmException;
+import java.security.UnrecoverableKeyException;
+import java.security.cert.CertificateException;
+
+import ssl.serversitessl.ServerSiteSSL;
+import ssl.verifyclient.VerifyClient;
+
+public class MainSSL {
+	
+	public static String QUEUE_NAME = "hello";
+
+	public static void main(String[] args) throws IOException, KeyManagementException, NoSuchAlgorithmException, InterruptedException, KeyStoreException, CertificateException, UnrecoverableKeyException {
+		
+		//To execute an example, un-comment one of the following lines:
+		
+		//ServerSiteSSL a = new ServerSiteSSL();
+		//VerifyClient b = new VerifyClient();
+	    
+	    
+	}
+
+}
